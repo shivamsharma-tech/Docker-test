61188f0cd7320bdba0e97c4d23438333a1db59fe
Shivam Sharma <shivamsharma3835@gmail.com>
2025-05-08 14:17:32 +0530

    ,

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! this for multi port')
+  res.send('Hello World! this for multi port which is run on 3000 and 8080 ')
 })
 
 app.listen(port, () => {
